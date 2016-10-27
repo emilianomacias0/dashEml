115eb219dd207a5e40f1d76d6edf5d5a6f3c10cb
emiliano macias <emilianomacias0@gmail.com>
2016-10-27 17:04:21 -0500

    Se agrego inicializador de cuenta de administrador

--- server/main.js
+++ server/main.js
@@ -1,15 +1,28 @@
 
 
 
-	// var emi = Accounts.createUser({
- //    profile: {
- //      name: 'Emiliano Macias'
- //    },
- //    username: "macias",
- //    email: "macias@mail.com",
- //    password: "macias",
- //  });
-// var em = Meteor.users.findOne({username: 'macias'})._id;
-// Roles.addUserToRoles( em,  ["admin"] );
+
+
+  if (Meteor.users.find({
+  //Crea el usuario admin 
+    }).count() == 0) {
+  
+    try{
+      var userId = Accounts.createUser({
+    profile: {
+      name: 'Administrador'
+    },
+    username: "admin",
+    email: "admin@mail.com",
+    password: "admin",
+  });
+  }catch(e){
+
+  }finally{
+     var em = Meteor.users.findOne({username: 'admin'})._id;
+    Roles.addUserToRoles( em,  ["admin"] );
+  }
+   console.log('Se agrego cuenta de administrador admin@mail.com/admin');
+   }
 
 
